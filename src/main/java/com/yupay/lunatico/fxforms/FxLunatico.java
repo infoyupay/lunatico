df1f20d859bec5d5ae97e094e9299e561ad43cdd
dvidal <dvidal@infoyupay.com>
2023-05-14 23:46:35 -0500

    Added a shared (Static) reference to the application controller for further usage.
    Signed-off-by: dvidal <dvidal@infoyupay.com>

--- src/main/java/com/yupay/lunatico/fxforms/FxLunatico.java
+++ src/main/java/com/yupay/lunatico/fxforms/FxLunatico.java
@@ -12,14 +12,29 @@ import org.jetbrains.annotations.NotNull;
  * @version 1.0
  */
 public class FxLunatico extends Application {
+    /**
+     * Holds application instance.
+     */
+    public static FxLunatico MY_APP;
+    /**
+     * Holds application main window controller instance.
+     */
+    public static LunaticoScene APP_CONTROLLER;
 
+    /**
+     * Entry point to the application.
+     *
+     * @param args command line arguments.
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(@NotNull Stage primaryStage) {
-        FxForms.lunaticoScene().show(primaryStage);
+        MY_APP = this;
+        APP_CONTROLLER = FxForms.lunaticoScene();
+        APP_CONTROLLER.show(primaryStage);
     }
 
     @Override
